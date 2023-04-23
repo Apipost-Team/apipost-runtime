887076a57bc1b774ba7af9308b8500a729f29688
trqdz <trqdz@hotmail.com>
2023-04-23 18:22:41 +0800

    fix:测试数据中有换行时数据解析异常的问题

--- runtime.js
+++ runtime.js
@@ -421,7 +421,8 @@ const Runtime = function ApipostRuntime(emitRuntimeEvent, enableUnSafeShell = tr
             }).join('|'), 'gi');
 
             variablesStr = _.replace(variablesStr, _regExp, (key) => {
-                const reStr = allVariables[String(_.replace(key, /[{}]/gi, ''))];
+                let reStr = allVariables[String(_.replace(key, /[{}]/gi, ''))];
+                reStr=reStr.replace(/\n/g,'\\n');      //bugfix v7.1.4
                 // console.log(String(_.replace(key, /[{}]/gi, '')), reStr, _regExp);
                 if (typeof reStr !== 'undefined') {
                     return reStr;
