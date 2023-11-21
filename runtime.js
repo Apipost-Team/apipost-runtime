500d7105c3fa74126df3ae0e2bde7137cb1e1fce
phpwechat <3257132998@qq.com>
2023-11-22 02:05:56 +0800

    fix: 数据库操作变量延迟生效的bug

--- runtime.js
+++ runtime.js
@@ -657,30 +657,63 @@ const Runtime = function ApipostRuntime(emitRuntimeEvent, enableUnSafeShell = tr
                           switch (_.toLower(item.type)) {
                             case 'database': // database 最终也是要转化成脚本
                               if (Number(item.enabled) > 0 && _.isObject(connectionConfigs[item.data?.connectionId])) {
-                                try {
-                                  let _db_res = await DatabaseQuery(connectionConfigs[item.data?.connectionId], mySandbox.replaceIn(item.data?.query)); // fix 执行语句没有进行变量替换的bug
 
+                                try {
+                                  let roadom = _.random(10, 1000000);
+
+                                  // 打印错误日志语句
+                                  let db_console_script = '';
                                   if (Number(item.data?.isConsoleOutput) > 0) {
-                                    _requestPara[_type] = `${_requestPara[_type]}\r\nconsole.log(${JSON.stringify(_db_res?.result)});`
+                                    db_console_script = `console.log(JSON.stringify(db_result_${roadom}?.result));`
                                   }
 
+                                  // 设置变量语句
+                                  let db_set_script = '';
                                   if (_.isArray(item.data?.variables)) {
                                     _.forEach(item.data?.variables, function (vars) {
-
-                                      let _var_val = '';
-
-                                      if (_.isObject(_db_res?.result)) {
-                                        _var_val = jsonpath.value(_db_res?.result, vars.pattern ? vars.pattern : '$');
+                                      let let_roadom = _.random(10, 1000000);
+                                      let _var_val_script = `\r\nlet _var_val_${let_roadom}='';\r\nif (_.isObject(db_result_${roadom}?.result)) {
+                                        _var_val_${let_roadom} = jsonpath.value(db_result_${roadom}?.result, ${vars.pattern} ? ${vars.pattern} : '$');
                                       } else {
-                                        _var_val = _db_res?.result;
-                                      }
+                                        _var_val_${let_roadom} = db_result_${roadom}?.result;
+                                      }`
 
-                                      _requestPara[_type] = `${_requestPara[_type]}\r\npm.${vars.type}.set("${vars.name}",${JSON.stringify(_var_val)});`
+                                      db_set_script = `\r\n${db_set_script}\r\n${_var_val_script}\r\npm.${vars.type}.set("${vars.name}",JSON.stringify(_var_val_${let_roadom}));`
                                     });
                                   }
-                                } catch (e) {
-                                  _requestPara[_type] = `${_requestPara[_type]}\r\nconsole.log("${e?.result}");`
-                                }
+
+                                  _requestPara[_type] = `${_requestPara[_type]}\r\ntry {
+                                    console.log("Query:" + apt.variables.replaceIn("${item.data?.query}"))
+                                    let db_result_${roadom} = await DatabaseQuery(${JSON.stringify(connectionConfigs[item.data?.connectionId])}, apt.variables.replaceIn("${item.data?.query}"));
+                                    ${db_console_script}
+                                    ${db_set_script}
+                                  } catch (e) { console.log(JSON.stringify(e)) }`;
+                                } catch (e) { }
+
+                                // try {
+                                //   let _db_res = await DatabaseQuery(connectionConfigs[item.data?.connectionId], mySandbox.replaceIn(item.data?.query)); // fix 执行语句没有进行变量替换的bug
+
+                                //   if (Number(item.data?.isConsoleOutput) > 0) {
+                                //     _requestPara[_type] = `${_requestPara[_type]}\r\nconsole.log(${JSON.stringify(_db_res?.result)});`
+                                //   }
+
+                                //   if (_.isArray(item.data?.variables)) {
+                                //     _.forEach(item.data?.variables, function (vars) {
+
+                                //       let _var_val = '';
+
+                                //       if (_.isObject(_db_res?.result)) {
+                                //         _var_val = jsonpath.value(_db_res?.result, vars.pattern ? vars.pattern : '$');
+                                //       } else {
+                                //         _var_val = _db_res?.result;
+                                //       }
+
+                                //       _requestPara[_type] = `${_requestPara[_type]}\r\npm.${vars.type}.set("${vars.name}",${JSON.stringify(_var_val)});`
+                                //     });
+                                //   }
+                                // } catch (e) {
+                                //   _requestPara[_type] = `${_requestPara[_type]}\r\nconsole.log("${e?.result}");`
+                                // }
                               }
                               break;
                             case 'assert': // 断言
